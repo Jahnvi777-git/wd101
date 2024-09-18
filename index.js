91088d9b992db5a0d67a97937d4a7685f8cb4dbc
Jahnvi777-git <148742397+Jahnvi777-git@users.noreply.github.com>
2024-09-18 14:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const displayEntries = () => {
             <th class="px-4 py-2">Email</th>
             <th class="px-4 py-2">Password</th>
             <th class="px-4 py-2">Dob</th>
-            <th class="px-4 py-2">Accepted Terms?</th>
+            <th class="px-4 py-2">Accepted terms?</th>
         </tr>
         ${tableEntries || ""}
     </table>`;
